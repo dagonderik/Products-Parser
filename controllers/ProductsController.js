c87a1bf7ede2c8a711ceaf5652bb8b44779178d9
dagon derik farias dantas <dagonderik@gmail.com>
2023-02-12 18:01:10 -0300

    Creates the get Details function that returns some data about the API and DB

--- controllers/ProductsController.js
+++ controllers/ProductsController.js
@@ -1,13 +1,18 @@
 import updateProductFields from "../components/UpdateProductFields.js";
 import Products from "../models/ProductsModel.js";
+import testConnection from "../test.js";
 
 export const getDetails = async (req, res) => {
-  // try {
-  //     const products = await Products.countDocuments();
-  //     res.json(products);
-  // } catch (error) {
-  //     res.status(500).json({message: error.message});
-  // }
+    const products = new Products(testConnection);
+  try {
+    await products.save();
+    await Products.deleteOne({ code: testConnection.code });
+    const lastUpdate = await Products.find().sort({ imported_t: -1}).limit(1);
+    console.log(lastUpdate);
+    res.status(200).send(`Write/Read channels ok! The last update was made at ${lastUpdate[0].imported_t}`);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 export const getProducts = async (req, res) => {
