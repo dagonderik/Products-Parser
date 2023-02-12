b5696170d38dad870bdefb1b8e209f97559aa180
dagon derik farias dantas <dagonderik@gmail.com>
2023-02-12 19:03:17 -0300

    Parses through the data chunk separating the objects and adding them to the DB as documents

---
+++ components/InsertNewDocument.js
@@ -0,0 +1,26 @@
+import findSubstring from "./FindSubstring.js";
+import axios from "axios";
+
+const insertNewDocument = async (dataString) => {
+  let newDocument = "";
+  let manipulatedString = dataString;
+  let counter = 1;
+
+  while (findSubstring(manipulatedString)) {
+    newDocument = findSubstring(manipulatedString);
+    console.log(`Object ${counter}`);
+    console.log("");
+    if (newDocument.slice(0, 1) == "{") {
+      const product = JSON.parse(newDocument);
+      console.log(newDocument);
+      await axios.put(
+        `http://localhost:5000/products/${product.code}`,
+        product
+      );
+      counter++;
+    }
+    manipulatedString = manipulatedString.slice(newDocument.length);
+  }
+};
+
+export default insertNewDocument;
